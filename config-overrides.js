69130f033ea383054a6bfa85fd578dc28dcf8312
akshaycj <akshaycj999@gmail.com>
2019-03-19 14:09:19 +0530

    Added Compatibility with CRA 2.0

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,9 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
-  return config;
-};
+const { override, fixBabelImports } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: "css"
+  })
+);
